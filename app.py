51828370a41fed9ec873db65d355745eaa8be24b
phanh1106 <anhmai2088@gmail.com>
2024-05-28 22:45:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import pandas as pd
 import numpy as np
 clf=pickle.load(open('bank_churn_1.pkl','rb'))
 st.title("Dự đoán sự rời bỏ của khách hàng ngân hàng")
-id=st.number_input("id khách",0,275056,1000)
-Age = st.number_input("Tuổi", 18, 40, 20)
-CreditScore = st.number_input("Điểm tín dụng", 0, 850, 600)
-Balance = st.number_input("Số dư tài khoản", 0.0, 121263.62, 50000.0)
-EstimatedSalary = st.number_input("Lương ước tính", 0.0,184866.69, 50000.0)
-prediction = clf.predict([[id,Age,CreditScore,Balance,EstimatedSalary]])
+CustomerId=st.number_input("CustomerId",0,275056,1000)
+#Age = st.number_input("Age", 18, 40, 20)
+CreditScore = st.number_input("CreditScore", 0, 850, 600)
+Balance = st.number_input("Balance", 0.0, 121263.62, 50000.0)
+EstimatedSalary = st.number_input("EstimatedSalary", 0.0,184866.69, 50000.0)
+prediction = clf.predict([[CustomerId,Age,CreditScore,Balance,EstimatedSalary]])
 d=pd.read_csv('train.csv')
 if st.button("Dự đoán"):
-    input_data = np.array([[id, Age, CreditScore, Balance, EstimatedSalary]])
+    input_data = np.array([[CustomerId, Age, CreditScore, Balance, EstimatedSalary]])
     prediction = clf.predict(input_data)
     predicted_class = "Rời bỏ" if prediction[0] == 1 else "Ở lại"
     st.write(f"Dự đoán: {predicted_class}")
