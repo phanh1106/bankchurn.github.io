4069d2b8c9e7027b413778f2a2c6efa5f77d8e05
phanh1106 <anhmai2088@gmail.com>
2024-05-28 22:33:34 +0700

    Update bank_churn.py

--- bank_churn.py
+++ bank_churn.py
@@ -1,18 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 import pickle as pickle
 import streamlit as st
 import numpy as np
-d= pd.read_csv('train.csv')
-feature=['id','Age','CreditScore','Balance','EstimatedSalary']
-target=['Exited']
-X=d[feature]
-y=d[target]
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 1/4, random_state=42,)
-clf = LogisticRegression()
-clf.fit(X_train, y_train)
+d = pd.read_csv('train.csv')
+
+feature = ['id', 'Age', 'CreditScore', 'Balance', 'EstimatedSalary']
+target = ['Exited']
+X = d[feature]
+y = d[target]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/4, random_state=42)
+
+clf = KNeighborsClassifier()
+clf.fit(X_train, y_train.values.ravel())
 
 print(clf.score(X_test, y_test))
 
