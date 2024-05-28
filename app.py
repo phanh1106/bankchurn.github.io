3a05c0df4d1dc56a5eb05a924af57003f10d35d6
phanh1106 <anhmai2088@gmail.com>
2024-05-28 22:41:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import pandas as pd
 import numpy as np
 clf=pickle.load(open('bank_churn_1.pkl','rb'))
 st.title("Dự đoán sự rời bỏ của khách hàng ngân hàng")
-id=st.number_input("id khách",0,82516,1002)
-Age = st.number_input("Tuổi", 18, 38, 30)
-CreditScore = st.number_input("Điểm tín dụng", 0, 656, 600)
-Balance = st.number_input("Số dư tài khoản", 0.0, 55478.086689, 50000.0)
-EstimatedSalary = st.number_input("Lương ước tính", 0.0, 112574.822734, 53213.0)
+id=st.number_input("id khách",0,275056,1000)
+Age = st.number_input("Tuổi", 18, 40, 20)
+CreditScore = st.number_input("Điểm tín dụng", 0, 850, 600)
+Balance = st.number_input("Số dư tài khoản", 0.0, 121263.62, 50000.0)
+EstimatedSalary = st.number_input("Lương ước tính", 0.0,184866.69, 50000.0)
 prediction = clf.predict([[id,Age,CreditScore,Balance,EstimatedSalary]])
 d=pd.read_csv('train.csv')
 if st.button("Dự đoán"):
